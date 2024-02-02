5f7a4280e7f621290f7e50f72a32b0a5e0f90a8e
donghyeon shin <donghyun4591@gmail.com>
2024-02-02 13:44:36 +0900

    skb ssh 11117포트 변경

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "ubuntu_instance" {
     instance_type = var.instance_type
     key_name      = var.key_name
 
+
+    # SKB SSH Port 22 -> 11117 Port로 변경 스크립트
+    # user_data = <<-EOF
+    #             #!/bin/bash
+    #             sed -i 's/#Port 22/Port 11117/g' /etc/ssh/sshd_config
+    #             systemctl restart sshd
+    #             EOF
+
     root_block_device {
         volume_size = var.root_volume_size
     }
